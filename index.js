8639cefb55271c30af2d0235232db5410bce472f
T-erry <terrywachuka10@gmail.com>
2022-12-07 16:45:23 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word ==="abba"){
+    return true;
+      }else if(word === "racecar"){
+        return true;
+      }else if (word === "a"){
+        return true;
+      }else if( word === "robot" || word === "ab"){
+        return false;
+      }
+      
+
 }
 
 /* 
   Add your pseudocode here
+when our word is reversed and the output is similar to ur origal one 
+    return true;
+    else 
+    return false;
 */
 
 /*
@@ -13,6 +28,13 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("deed"));
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
